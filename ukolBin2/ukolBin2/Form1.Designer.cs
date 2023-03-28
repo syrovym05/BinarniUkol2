79379f57dbc51c0642c939756c822e0d99ccbe00
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-03-28 21:32:43 +0200

    Add files via upload

---
+++ ukolBin2/ukolBin2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ukolBin2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vytvořit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(247, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 253);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ukolBin2/ukolBin2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukolBin2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.ShowIcon = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("sport.txt", Encoding.GetEncoding("Windows-1250"));
+            FileStream fs = new FileStream("sport.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs, Encoding.GetEncoding("Windows-1250"));
+            BinaryReader br = new BinaryReader(fs, Encoding.GetEncoding("Windows-1250"));
+
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split(';');
+
+                int osc = Convert.ToInt32(s[0]);
+                int vyska = Convert.ToInt32(s[4]);
+                int vaha = Convert.ToInt32(s[5]);
+                char pohlavi = Convert.ToChar(s[3]);
+
+                bw.Write(osc);
+                bw.Write(s[1]);
+                bw.Write(s[2]);
+                bw.Write(pohlavi);
+                bw.Write(vyska);
+                bw.Write(vaha);
+                //bw.Write("\n");
+            }
+
+                                   
+            br.BaseStream.Position = 0;
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                textBox1.Text += br.ReadInt32().ToString() + " " + br.ReadString() + " " + br.ReadString() + " " + br.ReadChar().ToString() + " " + br.ReadInt32().ToString() + " " + br.ReadInt32().ToString() + "\r\n";
+            }
+
+            sr.Close();
+            fs.Close();
+            bw.Close();            
+            br.Close();
+
+        }
+
+       
+    }
+}
